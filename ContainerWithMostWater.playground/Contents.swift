4199d8458d06a171795efea25d9dd7ab7fdbf447
Courser <wangzhongdi@meituan.com>
2020-06-01 21:10:04 +0800

    feature:update

--- ContainerWithMostWater.playground/Contents.swift
+++ ContainerWithMostWater.playground/Contents.swift
@@ -30,13 +30,25 @@ maxAreaA([1,8,6,2,5,4,8,3,7])
 
 func maxAreaB(_ height: [Int]) -> Int {
     
+    var maxArea = 0
     var leftPt = 0
     var rightPt = height.count - 1
     
+    while leftPt != rightPt {
+        let minHeight = min(height[leftPt], height[rightPt])
+        let area = minHeight * (rightPt - leftPt)
+        if area > maxArea {
+            maxArea = area
+        }
+        if minHeight == height[leftPt] {
+            leftPt += 1
+        } else {
+            rightPt -= 1
+        }
+    }
     
-    
+    return maxArea
 }
 
-func maxAreaRecurison
-
+maxAreaB([1,8,6,2,5,4,8,3,7])
 
